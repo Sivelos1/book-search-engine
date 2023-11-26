180bf3bf04d37effb2cb5ddf6627bff93c36efb3
Devin Amlen <devinamlen@gmail.com>
2023-11-26 12:45:15 -0500

    Update SignupForm.jsx

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -31,7 +31,7 @@ const SignupForm = () => {
     }
     try {
       const { data } = await addUser({
-        variables: { ...formState },
+        variables: { ...userFormData },
       });
 
       Auth.login(data.addUser.token);
